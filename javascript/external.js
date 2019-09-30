85a3d142223b6b88391cba98dccee726a1ff5b9d
cwr98 <cwr59@txstate.edu>
2019-09-30 15:53:30 -0500

    Completed Lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,5 @@
+//The const value behaves like a let varible only
+//it can not be reassigned
 //const x = "web";
 //const y = "mapping";
 //const out = x + y;
@@ -24,7 +26,14 @@
  //document.writeln("You chose Cancel!");
  //}
 //}
+//Display a prompt box which ask the user for her or his name,
+//and output a message
 user_name = window.prompt("Please enter your name", "Type your name here");
+// var is varible, the x is the ID with the unique name of "Hello welcome"
 var x = "Hello, Welcome ";
+//The step adds var or x with the text user_name.
+//the =welcome_message is combineing the var and text
 welcome_message = x+user_name;
+//This method writes HTML expressions or JS code
+// to a document
 document.write(welcome_message);

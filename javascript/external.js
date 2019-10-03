e7d11ecad1ab68a0b5a76736882838e5759d643e
cwr98 <cwr59@txstate.edu>
2019-10-03 14:52:20 -0500

    Filled Content and imporved web map table asthetics

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,15 @@
+var webmaps =
+  [
+    ["Democratic Primary Fundraising",
+    "https://www.nytimes.com/interactive/2019/08/02/us/politics/2020-democratic-fundraising.html",
+    "The map allows us to see where the candidates are getting there funding, this is significant because many of them have yet to break out of there own states or local areas. This application was made for political analyst who are interested in seeing a graphical representation of who is getting the most funding."
+  ],
+  ["Highly Detailed 2016 Election Map",
+  "https://www.nytimes.com/interactive/2018/upshot/election-2016-voting-precinct-maps.html",
+  "The propose of this map is to show the user how their home area voted in the 2016 presidential election. This allows us to see exactly how much each candidate took each county by. It could be used for spatial analysis of different voting patterns in the states that flipped."
+  ]
+];
+
 function welcome()
   {
     let a = "Please enter your name.";
@@ -9,24 +21,29 @@ function welcome()
     return message
   }
 
+
   function webmap_table()
   {
-    document.write("<table width=100%");
-    for (var row=0; row < 2; row++)
+    document.write("<table width=100%>");
+    for (var row=0; row < webmaps.length; row++)
     {
     document.write("<tr>");
-    for (var column=0; column < 3; column++)
+    for (var column=0; column < webmaps[0].length; column++)
       {
-        document.write("<td>" + row + "," + column + "</td>");
+        if (column < 2){
+          document.write("<td>" + webmaps[row][column] + "</td>");
+        }
+        else{
+          document.write("</tr>");
+          document.write("<tr>");
+          document.write("<td colspan='2'>" + webmaps[row][column] + "<p></td>");
+        }
       }
       document.write("</tr>");
+
     }
     document.write("</table>");
   }
-  
-
-
-
 
 //The const value behaves like a let varible only
 //it can not be reassigned
